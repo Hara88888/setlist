99ae8edc3bbc6996c2f0de60863854ef1b2b1e21
Hara88888 <g031t126@s.iwate-pu.ac.jp>
2023-12-05 12:21:35 +0900

    マイグレーション、シーダ,ルートの修正

--- database/migrations/2023_10_05_000008_create_music_setlist_table.php
+++ database/migrations/2023_10_05_000008_create_music_setlist_table.php
@@ -14,13 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('music_setlist', function (Blueprint $table) {
-            $table->ForeignId('setlist_id')->constrained();
-            $table->ForeignId('music_id')->constrained("musics");
+            $table->ForeignId('setlist_id')->constrained() -> cascadeOnDelete();
+            $table->ForeignId('music_id')->constrained("musics") -> cascadeOnDelete();
             $table->primary(['setlist_id','music_id']);
             $table->integer('song_order');
             $table->string('live_memo',200)->nullable();
-            $table->foreign('setlist_id')->references('id')->on('setlists')->onDelete('cascade');
-            $table->foreign('music_id')->references('id')->on('musics')->onDelete('cascade');
             $table->timestamps();
         });
     }
